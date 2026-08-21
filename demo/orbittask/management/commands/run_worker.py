e39155f3dc7d851dd67ebb6d185ed7f7ac9a1d65
arvinrjb <arvinrjb13@gmail.com>
2026-08-21 10:47:13 +0330

    feat: worker
    add status, start time, finished time, result, error, retries and save
    to database

--- demo/orbittask/management/commands/run_worker.py
+++ demo/orbittask/management/commands/run_worker.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 from redis.exceptions import TimeoutError as RedisTimeoutError
 from concurrent.futures import ThreadPoolExecutor
 from orbittask.models import Task
@@ -6,18 +7,66 @@ from orbittask.conf import get_redis
 from orbittask.registry import TASK_registery
 
 
-redis = get_redis()
-
 def get_task(id):
-    return Task.objects.get(id=id)
+    try:
+        obj = Task.objects.get(id=id)
+        func = TASK_registery.get(obj.name)
+        return obj, func
+    except Task.DoesNotExist:
+        raise Exception("Error while get Task")
 
 def run_task(task_id):
-    task = get_task(int(task_id))
-    func = TASK_registery.get(task.name)
-    func(*task.args, **task.kwargs)
+    task, func = get_task(task_id)
+    task.status = "RUNNING"
+    task.started_at = timezone.now()
+    task.save(
+        update_fields=[
+            "status",
+            "started_at"
+        ]
+    )
+
+    result = None
+    for _ in range(task.max_retries):
+        try:
+            result = func(*task.args, **task.kwargs)
+            break
+        except:
+            task.retries+=1
+            continue
+
+    task.finished_at = timezone.now()
+
+    if result is None:
+        task.status = "FAILED"
+        task.error = f"Error, task: {task.name}"
+        task.save(
+            update_fields=[
+                "status",
+                "error",
+                "retries",
+                "started_at",
+                "finished_at",
+            ]
+        )
+    else:
+        task.status = "SUCCESS"
+
+    task.result = str(result)
+    task.save(
+        update_fields=[
+            "status",
+            "retries",
+            "result",
+            "started_at",
+            "finished_at"
+        ]
+    )
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        redis = get_redis()
         self.stdout.write("Worker started. Waiting for tasks...")
         self.stdout.write(f"Registered tasks: {list(TASK_registery.keys())}")
         with ThreadPoolExecutor(max_workers=4) as executor:
