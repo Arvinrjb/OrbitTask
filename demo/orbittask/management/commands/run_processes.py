3fb7c286868c46777d0067267c6501c838c63870
arvinrjb <arvinrjb13@gmail.com>
2026-08-31 12:40:06 +0330

    fix: fix django import and setup

--- demo/orbittask/management/commands/run_processes.py
+++ demo/orbittask/management/commands/run_processes.py
@@ -1,15 +1,16 @@
 import json
 from django.core.management.base import BaseCommand
-from django.utils import timezone
 from redis.exceptions import TimeoutError as RedisTimeoutError
 from concurrent.futures import ProcessPoolExecutor
-from orbittask.models import Task
 from orbittask.conf import get_redis
 from orbittask.registry import TASK_registery_process
 
 
 # get object from database and get functions from TASK_registery 
 def get_task(id):
+    import django
+    django.setup()
+    from orbittask.models import Task
     try:
         obj = Task.objects.get(id=id)
         func = TASK_registery_process.get(obj.registry)
@@ -19,6 +20,7 @@ def get_task(id):
 
 # run task function
 def run_task(task_id):
+    from django.utils import timezone
     task, func = get_task(task_id)
     task.status = "RUNNING"
     task.started_at = timezone.now()
