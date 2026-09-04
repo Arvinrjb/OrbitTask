5e5d72b1d0cba29bc2c2633db55e67a2e25c4505
arvinrjb <arvinrjb13@gmail.com>
2026-09-04 15:51:38 +0330

    feat: add eta for CPU Bound Tasks

--- demo/orbittask/management/commands/run_processes.py
+++ demo/orbittask/management/commands/run_processes.py
@@ -1,28 +1,60 @@
+import threading
 import json
+from django.utils.dateparse import parse_datetime
+from django.utils import timezone
+from orbittask.registry import TASK_registery_process
 from django.core.management.base import BaseCommand
 from redis.exceptions import TimeoutError as RedisTimeoutError
 from concurrent.futures import ProcessPoolExecutor
 from orbittask.conf import get_redis
-from orbittask.registry import TASK_registery_process
+
+redis = get_redis()
+threading_event = threading.Event()
+
+def delay_thread():
+    import json
+    from orbittask.conf import get_redis
+    redis = get_redis()
+    while not threading_event.is_set():
+        now = timezone.now().timestamp()     
+        tasks = redis.zrangebyscore(
+            "orbittask:delayed:process",
+            min=0,
+            max=now
+        )
+        
+        pipe = redis.pipeline(transaction=True)
+        for row in tasks:
+            task = json.loads(row)
+            task["eta"] = None
+            pipe.zrem("orbittask:delayed:process", row)
+            pipe.lpush("orbittask:queue:process", json.dumps(task))
+        pipe.execute()        
+        threading_event.wait(timeout=1)
 
 
-# get object from database and get functions from TASK_registery 
-def get_task(id):
-    import django
-    django.setup()
-    from orbittask.models import Task
-    try:
-        obj = Task.objects.get(id=id)
-        func = TASK_registery_process.get(obj.registry)
-        return obj, func
-    except Task.DoesNotExist:
-        raise Exception("Error while get Task")
+def start_delay_thread():
+    import threading
+    thread = threading.Thread(target=delay_thread)
+    thread.start()
+    return thread
+
 
 # run task function
 def run_task(task_id):
+    import django
+    django.setup()
+    from orbittask.models import Task
     from orbittask.models import Logs
     from django.utils import timezone
-    task, func = get_task(task_id)
+    from orbittask.registry import TASK_registery_process
+
+    try:
+        task = Task.objects.get(id=task_id)
+        func = TASK_registery_process.get(task.registry)
+    except Task.DoesNotExist:
+        raise Exception("Error while get Task")
+
     Logs.objects.create(
         task=task,
         detail="Task Running",
@@ -96,22 +128,30 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        thread = start_delay_thread() 
         process=options["process"]
-        redis = get_redis()
         self.stdout.write("Worker started. Waiting for tasks...")
         self.stdout.write(f"Registered tasks: {list(TASK_registery_process.keys())}")
-        # for I/O bound tasks
-        with ProcessPoolExecutor(max_workers=process) as executor:
-            while True:
-                try:
-                    task_redis = redis.brpop("orbittask:queue:process", timeout=5)
-                except RedisTimeoutError:
-                    continue
+        # for CPU bound tasks
+        try:
+            with ProcessPoolExecutor(max_workers=process) as executor:
+                while True:
+                    try:
+                        task_redis = redis.brpop("orbittask:queue:process", timeout=5)
+                    except RedisTimeoutError:
+                        continue
 
-                if task_redis is None:
-                    continue
-                
-                message = json.loads(task_redis[1])
-                task_id = message["id"]
-                executor.submit(run_task, task_id)
+                    if task_redis is None:
+                        continue
+                    
+                    message = json.loads(task_redis[1])
+                    task_id = message["id"]
+                    if message["eta"]:
+                        eta_timestamp = parse_datetime(message["eta"]).timestamp()
+                        redis.zadd("orbittask:delayed:process", {json.dumps(message):eta_timestamp})
+                    else:
+                        executor.submit(run_task, task_id)
+        except:
+            threading_event.set()
+            thread.join()
 
